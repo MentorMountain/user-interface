350d2d2bfb0ec663e6b8a24dc43255a4d97edb9e
Jarod <jarodf@sfu.ca>
2023-03-10 21:39:03 -0800

    ui: initial commit for files and basic layout

---
+++ temp_index/api_calls.js
@@ -0,0 +1,23 @@
+function blogAdd() {
+    document.getElementById('add-result').textContent = "ADD function not implemented yet";
+    document.getElementById('add-result').style.color = "red";
+    console.warn("Add not implemented yet");
+}
+
+function blogGet() {
+    document.getElementById('get-result').textContent = "GET function not implemented yet";
+    document.getElementById('get-result').style.color = "red";
+    console.warn("Get not implemented yet");
+}
+
+function blogList() {
+    document.getElementById('list-result').textContent = "LIST function not implemented yet";
+    document.getElementById('list-result').style.color = "red";
+    console.warn("List not implemented yet");
+}
+
+function blogDelete() {
+    document.getElementById('delete-result').textContent = "DELETE function not implemented yet";
+    document.getElementById('delete-result').style.color = "red";
+    console.warn("Delete not implemented yet");
+}
